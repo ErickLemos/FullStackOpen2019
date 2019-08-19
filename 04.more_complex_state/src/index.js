f623e297d77c626c75b01d11e63a31a5751fb629
Erick B. Lemos <ericklemoscosta@gmail.com>
2019-08-19 10:36:48 -0300

    part1: 7

--- 04.more_complex_state/src/index.js
+++ 04.more_complex_state/src/index.js
@@ -24,6 +24,9 @@ const App = () => {
             <p>Good: {good}</p>
             <p>Neutral: {neutral}</p>
             <p>Bad: {bad}</p>
+            <p>All: {good + neutral + bad}</p>
+            <p>Average: {good - bad}</p>
+            <p>Positive: {good * 100 / (good + neutral + bad)}</p>
         </div>
     )
 };
