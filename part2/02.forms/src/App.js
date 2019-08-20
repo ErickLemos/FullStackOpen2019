78aa063f9aaff85ca4df3ba1a1ed07c9c4a11e98
Erick B. Lemos <ericklemoscosta@gmail.com>
2019-08-20 08:24:56 -0300

    part2: 6

--- part2/02.forms/src/App.js
+++ part2/02.forms/src/App.js
@@ -1,21 +1,43 @@
 import React, {useState} from 'react'
+import Person from "./components/Person";
 
 const App = () => {
-    const [persons, setPersons] = useState([{name: 'Arto Hellas'}]);
+    const [persons, setPersons] = useState([{name: 'Arto Hellas', id: 1}]);
     const [newName, setNewName] = useState('');
+
+    const handleNameChange = (event) => {
+        console.log(event.target.value);
+        setNewName(event.target.value);
+    };
+
+    const addPerson = (event) => {
+        event.preventDefault();
+        const personObject = {
+            name: newName,
+            id: persons.length + 1
+        };
+        setPersons(persons.concat(personObject));
+        setNewName('');
+        console.log(persons);
+    };
+
+    const rows = () => persons.map(person => {
+        return <Person key={person.id} person={person}/>
+    });
 
     return (
         <div>
             <h2>Phonebook</h2>
-            <form>
+            <form onSubmit={addPerson}>
                 <div>
-                    name: <input/>
+                    name: <input value={newName} onChange={handleNameChange}/>
                 </div>
                 <div>
                     <button type="submit">add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
+            {rows()}
         </div>
     )
 };
