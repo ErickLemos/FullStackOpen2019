fbb86d38a9fe7bf00c80e4cd9649d910489e874d
Erick B. Lemos <ericklemoscosta@gmail.com>
2019-08-21 09:36:35 -0300

    part2: 15

--- part2/04.altering_data_in_server/src/services/PersonService.js
+++ part2/04.altering_data_in_server/src/services/PersonService.js
@@ -3,21 +3,23 @@ const baseUrl = 'http://localhost:3001/persons';
 
 const getAll = () => {
     const request = axios.get(baseUrl);
-    return request.then(response => response.data);
+    return request
+        .then(response => response.data)
+        .catch(() => console.log('fail'));
 };
 
 const create = newObject => {
     const request = axios.post(baseUrl, newObject);
-    return request.then(response => response.data);
+    return request
+        .then(response => response.data)
+        .catch(() => console.log('fail'));
 };
 
 const update = (id, newObject) => {
     const request = axios.put(`${baseUrl}/${id}`, newObject);
-    return request.then(response => response.data);
+    return request
+        .then(response => response.data)
+        .catch(() => console.log('fail'));
 };
 
-export default {
-    getAll: getAll,
-    create: create,
-    update: update
-}
+export default {getAll, create, update}
