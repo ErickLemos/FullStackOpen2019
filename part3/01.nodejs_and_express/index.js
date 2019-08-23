d5083d850bc41f71bf5002f7d600935c6e8718e5
Erick B. Lemos <ericklemoscosta@gmail.com>
2019-08-23 11:23:56 -0300

    part3: 1

---
+++ part3/01.nodejs_and_express/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+let persons = [
+    {
+        "name": "a",
+        "number": "123",
+        "id": 1
+    },
+    {
+        "name": "b",
+        "number": "12",
+        "id": 2
+    },
+    {
+        "name": "c",
+        "number": "123213",
+        "id": 3
+    },
+    {
+        "name": "v",
+        "number": "123",
+        "id": 4
+    },
+    {
+        "name": "ab",
+        "number": "asd",
+        "id": 5
+    },
+    {
+        "name": "banana",
+        "number": "12",
+        "id": 6
+    }
+];
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons);
+});
+
+// app.get('/api/persons/:id', (req, res) => {
+//     const id = Number(req.params.id);
+//     const person = persons.find(person => person.id === id);
+//
+//     if (person) {
+//         res.json(person)
+//     } else {
+//         res.status(404).end()
+//     }
+// });
+
+const port = 3001;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+});
