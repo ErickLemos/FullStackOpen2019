1f243e2dd48767055593f62f5951e0d7a1dfd890
Erick B. Lemos <ericklemoscosta@gmail.com>
2019-08-23 12:39:24 -0300

    part3: 4

--- part3/01.nodejs_and_express/index.js
+++ part3/01.nodejs_and_express/index.js
@@ -50,11 +50,18 @@ app.get('/api/persons/:id', (req, res) => {
      const person = persons.find(person => person.id === id);
 
 
-          if (person) {
+     if (person) {
          res.json(person)
      } else {
          res.status(404).end()
      }
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    persons = persons.filter(person => person.id !== id);
+
+    res.status(204).end();
 });
 
 const port = 3001;
