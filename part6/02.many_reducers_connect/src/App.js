eb0ca7d3b623ba850fca866400d82daf66d1fc6a
Erick B. Lemos <ericklemoscosta@gmail.com>
2019-10-24 16:00:06 -0300

    part6: ...

---
+++ part6/02.many_reducers_connect/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AnecdoteForm from "./components/AnecdoteForm";
+import AnecdoteList from "./components/AnecdoteList";
+
+const App = (props) => {
+
+    return (
+        <div>
+            <AnecdoteList props={props}/>
+            <AnecdoteForm props={props}/>
+        </div>
+    );
+};
+
+export default App
