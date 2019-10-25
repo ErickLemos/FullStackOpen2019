7f026b3dfac46bac768b5413b231cfead331610e
Erick B. Lemos <ericklemoscosta@gmail.com>
2019-10-25 15:23:34 -0300

    part6: 11

--- part6/02.many_reducers_connect/src/App.js
+++ part6/02.many_reducers_connect/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import Notification from "./components/Notification"
+import Filter from "./components/Filter";
 
 const App = (props) => {
-    const store = props.store
+    const store = props.store;
 
     return (
         <div>
+            <Filter props={store}/>
             <Notification props={store}/>
             <AnecdoteList props={store}/>
             <AnecdoteForm props={store}/>
--- part6/02.many_reducers_connect/src/index.js
+++ part6/02.many_reducers_connect/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {createStore, combineReducers} from 'redux'
+import {combineReducers, createStore} from 'redux'
 import App from './App'
 import anecdoteReducer from './reducers/anecdoteReducer'
 import notificationReducer from './reducers/notificationReducer'
+import filterReducer from './reducers/filterReducer'
 
 const reducer = combineReducers({
     anecdotes: anecdoteReducer,
-    notification: notificationReducer
-})
+    notification: notificationReducer,
+    filter: filterReducer
+});
 
 const store = createStore(reducer);
 
